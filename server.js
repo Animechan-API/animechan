5c1ff29f9c1e13d0c88d6178fd1471017d06d416
srocktim61 <srocktim61@gmail.com>
2020-03-06 12:13:20 +0530

    data samples added :tada:

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const db = require('./data/data.json');
 
 app.get('/', (req, res) => {
     res.send('quality anime quotes coming soon 🚀')
 })
 
+// get a random quote
+app.get('/api/quote', (req, res) => {
+    const quote = db[Math.floor(Math.random() * db.length)]
+    res.json(quote)
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`server is connected at ${process.env.PORT}`);
