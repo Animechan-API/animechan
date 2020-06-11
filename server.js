ab5ca86cddf6cdf868efda317298ef0ee1690f07
srocktim61 <srocktim61@gmail.com>
2020-06-11 20:02:49 +0530

    cors enabled :tada:

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
+const cors = require("cors");
 
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, './public')));
+app.use(cors());
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "./public")));
 
 const db = mongoose.connection;
 
 mongoose.connect(process.env.URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+db.once("open", () => console.log("mongodb is connected 🚀"));
+db.on("error", console.error.bind(console, "connection error:"));
 
-db.once('open', () => console.log('mongodb is connected 🚀'));
-db.on('error', console.error.bind(console, 'connection error:'));
-
-
-
-app.set('json spaces', 2);
+app.set("json spaces", 2);
 
 // index routes
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 // api routes
-app.use('/api', require('./routes/api/index'));
+app.use("/api", require("./routes/api/index"));
 // not found route
-app.use('*', require('./controllers/index_controllers').notFound);
+app.use("*", require("./controllers/index_controllers").notFound);
 
 app.listen(process.env.PORT, () => {
-    console.log(`server is connected at ${process.env.PORT} 🚀`);
-})
+  console.log(`server is connected at ${process.env.PORT} 🚀`);
+});
