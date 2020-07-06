55c533349f27d9464493772a049471ad3aae0f9b
rocktimsaikia <srocktim61@gmail.com>
2020-07-06 17:06:20 +0530

    query through character name is availabe :rocket:
    radomized the returning the quotes

--- controllers/api_controllers.js
+++ controllers/api_controllers.js
@@ -1,56 +1,61 @@
-const { slugGen } = require('../util/slugGen.js');
-const { matchAnime, matchChar, defaultFetch, pagination, radomQuery } = require('../util/query')
+const { slugGen } = require("../util/slugGen.js");
+const {
+  matchAnime,
+  matchChar,
+  defaultFetch,
+  pagination,
+  radomQuery,
+} = require("../util/query");
 
 module.exports.quotes = (req, res) => {
-    const { page, anime } = req.query;
+  const { page, anime, char } = req.query;
 
+  if (Object.keys(req.query).length === 0) {
+    defaultFetch().then((db) => {
+      res.json(db);
+    });
+  }
 
-    if (Object.keys(req.query).length === 0) {
-        defaultFetch().then(db => {
-            res.json(db);
-        })
+  /**
+   * query through anime name
+   */
+  if (anime) {
+    const slug = slugGen(anime);
+    matchAnime(slug).then((db) => {
+      res.json(db);
+    });
+  }
+
+  /**
+   * query through character name
+   */
+  if (char) {
+    const slug = slugGen(char);
+    matchChar(slug).then((db) => {
+      res.json(db);
+    });
+  }
+
+  /**
+   * if pagination is not specified return 10 quotes as default
+   * (pagination limit is up to 10)
+   */
+  if (page) {
+    if (page <= 10 && page > 0) {
+      pagination(page).then((db) => {
+        res.json(db);
+      });
+    } else {
+      res.json({
+        message: "page request limit is only up to 10!",
+        error: true,
+      });
     }
-
-    /**
-     * query through anime name
-     */
-    if (anime) {
-        const slug = slugGen(anime);
-        matchAnime(slug).then(db => {
-            res.json(db);
-        })
-    };
-
-    /**
-    * query through character name
-    */
-    // if (char) {
-    //     const slug = slugGen(char);
-    //     matchChar(slug).then(db => {
-    //         res.json(db);
-    //     })
-    // };
-
-    /** 
-     * if pagination is not specified return 10 quotes as default
-     * (pagination limit is up to 10)
-     */
-    if (page) {
-        if (page <= 10 && page > 0) {
-            pagination(page).then(db => {
-                res.json(db);
-            })
-        } else {
-            res.json({
-                message: 'page request limit is only up to 10!',
-                error: true
-            });
-        };
-    }
-}
+  }
+};
 
 module.exports.randomQuote = (req, res) => {
-    radomQuery().then(db => {
-        res.json(db)
-    })
-}
+  radomQuery().then((db) => {
+    res.json(db);
+  });
+};
