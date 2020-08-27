05ee266d1c16ca3e1e4c886381907f2a51ef271f
rocktimsaikia <srocktim61@gmail.com>
2020-08-28 00:59:02 +0530

    :sparkles: fix: fix unhandles promise warnings in productions

--- controllers/index.js
+++ controllers/index.js
@@ -5,60 +5,63 @@ const {modelFormat, slugGen, shuffleArray} = require('./util');
 
 module.exports = {
 
-	async getQuotes(req, res) {
+	getQuotes(req, res) {
 		const {page, anime, char} = req.query;
 
 		try {
 			// If no query is being passed then send default 10 quotes
 			if (Object.keys(req.query).length === 0) {
-				const data = await schema.aggregate([{$project: modelFormat}]).limit(10);
-				res.json({
-					status: 'ok',
-					statusCode: 200,
-					data
-				});
-			}
-
-			if (anime) {
-				const slug = slugGen(anime);
-				const db = await schema.aggregate([
-					{$match: {'anime.anime_slug': slug}},
-					{$project: modelFormat}
-				]);
-				const data = shuffleArray(db);
-				res.json({
-					status: 'ok',
-					statusCode: 200,
-					data
-				});
-			}
-
-			if (char) {
-				const slug = slugGen(char);
-				const db = await schema.aggregate([
-					{$match: {'char.char_slug': slug}},
-					{$project: modelFormat}
-				]);
-				const data = shuffleArray(db);
-				res.json({
-					status: 'ok',
-					statusCode: 200,
-					data
-				});
-			}
-
-			if (page) {
-				if (page <= 10 && page > 0) {
-					const data = await schema
-						.aggregate([{$project: modelFormat}])
-						.skip(10 * (page - 1))
-						.limit(10);
-
+				schema.aggregate([{$project: modelFormat}]).limit(10).then(data => {
 					res.json({
 						status: 'ok',
 						statusCode: 200,
 						data
 					});
+				});
+			}
+
+			if (anime) {
+				const slug = slugGen(anime);
+				schema.aggregate([
+					{$match: {'anime.anime_slug': slug}},
+					{$project: modelFormat}
+				]).then(db => {
+					const data = shuffleArray(db);
+					res.json({
+						status: 'ok',
+						statusCode: 200,
+						data
+					});
+				});
+			}
+
+			if (char) {
+				const slug = slugGen(char);
+				schema.aggregate([
+					{$match: {'char.char_slug': slug}},
+					{$project: modelFormat}
+				]).then(db => {
+					const data = shuffleArray(db);
+					res.json({
+						status: 'ok',
+						statusCode: 200,
+						data
+					});
+				});
+			}
+
+			if (page) {
+				if (page <= 10 && page > 0) {
+					schema
+						.aggregate([{$project: modelFormat}])
+						.skip(10 * (page - 1))
+						.limit(10).then(data => {
+							res.json({
+								status: 'ok',
+								statusCode: 200,
+								data
+							});
+						});
 				} else {
 					res.json({
 						message: 'page request limit is only up to 10!',
@@ -73,16 +76,17 @@ module.exports = {
 	},
 
 	// Get one random quote
-	async randomQuote(res) {
+	randomQuote(res) {
 		try {
-			const data = await schema.aggregate([
+			schema.aggregate([
 				{$sample: {size: 1}},
 				{$project: modelFormat}
-			]);
-			res.json({
-				status: 'ok',
-				statusCode: 200,
-				data
+			]).then(data => {
+				res.json({
+					status: 'ok',
+					statusCode: 200,
+					data
+				});
 			});
 		} catch (error) {
 			console.error(error);
