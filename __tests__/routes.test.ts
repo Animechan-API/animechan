b7111aeebac856356eb17cbf10e936973e1472c0
melosh <milasholsting@protonmail.com>
2022-03-20 23:15:09 +0100

    add test for every endpoint
    added tests for every endpoint.

--- __tests__/routes.test.ts
+++ __tests__/routes.test.ts
@@ -32,3 +32,82 @@ test('Get a random quote', async () => {
 	expect(response.body[0]).toHaveProperty('character');
 	expect(response.body[0]).toHaveProperty('anime');
 });
+
+test('Get a quotes from character', async () => {
+	const response = await request(server)
+		.get('/quotes/character?name=Father%20Willibald')
+		.expect('Content-type', /json/)
+		.expect(200);
+	expect(response.body).toBeDefined();
+	expect(Array.isArray(response.body)).toBeTruthy();
+	expect(response.body.length).toBeGreaterThanOrEqual(1)
+	expect(response.body[0]).toHaveProperty('text');
+	expect(response.body[0]).toHaveProperty('character');
+	expect(response.body[0]).toHaveProperty('anime');
+	expect(response.body[0].character).toBe('Father Willibald');
+});
+
+test("get a quotes from an Anime", async () => {
+	const response = await request(server)
+		.get('/quotes/anime?title=guilty%20crown')
+		.expect('Content-type', /json/)
+		.expect(200);
+	expect(response.body).toBeDefined();
+	expect(Array.isArray(response.body)).toBeTruthy();
+	expect(response.body.length).toBeGreaterThanOrEqual(1)
+	expect(response.body[0]).toHaveProperty('text');
+	expect(response.body[0]).toHaveProperty('character');
+	expect(response.body[0]).toHaveProperty('anime');
+	expect(response.body[0].anime).toBe('Guilty Crown');
+});
+
+
+test('Get a random quote from character', async () => {
+	const response = await request(server)
+		.get('/random/character?name=Father%20Willibald')
+		.expect('Content-type', /json/)
+		.expect(200);
+	expect(response.body).toBeDefined();
+	expect(Array.isArray(response.body)).toBeTruthy();
+	expect(response.body).toHaveLength(1);
+	expect(response.body[0]).toHaveProperty('text');
+	expect(response.body[0]).toHaveProperty('character');
+	expect(response.body[0]).toHaveProperty('anime');
+	expect(response.body[0].character).toBe('Father Willibald');
+});
+
+test("get a random quote from an Anime", async () => {
+	const response = await request(server)
+		.get('/random/anime?title=guilty%20crown')
+		.expect('Content-type', /json/)
+		.expect(200);
+	expect(response.body).toBeDefined();
+	expect(Array.isArray(response.body)).toBeTruthy();
+	expect(response.body).toHaveLength(1);
+	expect(response.body[0]).toHaveProperty('text');
+	expect(response.body[0]).toHaveProperty('character');
+	expect(response.body[0]).toHaveProperty('anime');
+	expect(response.body[0].anime).toBe('Guilty Crown');
+});
+
+test("get available quotes by anime", async () => {
+	const response = await request(server)
+		.get('/available/anime?title=guilty%20crown')
+		.expect('Content-type', /json/)
+		.expect(200);
+	expect(response.body).toBeDefined();
+	expect(response.body).toHaveProperty('anime');
+	expect(response.body).toHaveProperty('counts');
+	expect(response.body.anime).toBe('guilty crown');
+});
+
+test("get available quotes by character", async () => {
+	const response = await request(server)
+		.get('/available/character?name=Father%20Willibald')
+		.expect('Content-type', /json/)
+		.expect(200);
+	expect(response.body).toBeDefined();
+	expect(response.body).toHaveProperty('character');
+	expect(response.body).toHaveProperty('counts');
+	expect(response.body.character).toBe('Father Willibald');
+});
