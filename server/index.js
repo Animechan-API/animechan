ea3cb53f52a07d6860d7f007ab8180551f858c55
Rocktim Saikia <rocktimthedev@gmail.com>
2023-12-22 11:41:23 +0530

    chore: update scripts, dependencies, and env config

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ const database = require('./config/mongo');
 
 const MONGO_URI =
 	process.env.NODE_ENV !== 'production'
-		? process.env.MONGO_URI_DEV
+		? process.env.MONGO_READ_WRITE_URI_DEV
 		: process.env.MONGO_READ_URI_PROD;
 
-database.connect(MONGO_URI);
-console.log(`Connected to database successfully on ${MONGO_URI} 🚀`);
+database.connect(MONGO_URI).then(() => {
+	console.log(`Connected to database successfully on ${MONGO_URI} 🚀`);
+});
 
 server.listen(process.env.PORT, () => {
 	console.log(`Live at http://localhost:${process.env.PORT} 🚀`);
