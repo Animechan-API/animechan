97c9ba7dbf4bd864dafd86708e08924c3d3237f6
melosh101 <milasholsting@protonmail.com>
2023-12-24 05:51:03 +0100

    add basic ratelimits

--- server/config/server.js
+++ server/config/server.js
@@ -7,9 +7,13 @@ const session = require('express-session');
 const passport = require('passport');
 const initializePassport = require('./passport');
 const morgan = require('morgan');
+const ratelimit = require("koa-ratelimit")
+
 
 const app = express();
 const router = require('../routes');
+const limitDB = new Map();
+
 
 app.use(helmet());
 app.use(cors());
@@ -18,6 +22,14 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(ratelimit({
+	driver: "memory",
+	db: limitDB,
+	errorMessage: "Hold up, The characters behind the scenes cant keep coming up with quotes",
+	id: (ctx) => ctx.ip,
+	max: 100
+}))
 
 app.use(
 	session({
