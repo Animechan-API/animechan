0977c895931a9d7b1eb97ae84e0e7a7367e2b6fa
Rocktim Saikia <rocktimthedev@gmail.com>
2023-12-24 11:17:17 +0530

    refactor(server): replace koa-ratelimit with express-rate-limit

--- server/config/server.js
+++ server/config/server.js
@@ -7,29 +7,28 @@ const session = require('express-session');
 const passport = require('passport');
 const initializePassport = require('./passport');
 const morgan = require('morgan');
-const ratelimit = require("koa-ratelimit")
-
+const { rateLimit } = require('express-rate-limit');
 
 const app = express();
 const router = require('../routes');
-const limitDB = new Map();
-
 
 app.use(helmet());
 app.use(cors());
+
+app.use(
+	rateLimit({
+		windowMs: 60 * 60 * 1000, // 1 hour window
+		max: 5, // Limit each IP to 5 requests per hour
+		standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+		legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+		message: 'Hold up, The characters behind the scenes cant keep coming up with quotes',
+	})
+);
+
 app.use(compression());
 app.use(morgan('dev'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(ratelimit({
-	driver: "memory",
-	db: limitDB,
-	errorMessage: "Hold up, The characters behind the scenes cant keep coming up with quotes",
-	id: (ctx) => ctx.ip,
-	max: 100
-}))
 
 app.use(
 	session({
