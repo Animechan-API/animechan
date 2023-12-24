7add70b8b8b6f97b78e8fd60c310f334fe5ae852
Rocktim <rocktimthedev@gmail.com>
2023-12-24 12:39:47 +0530

    add basic ratelimit (#86)
    Co-authored-by: melosh101 <milasholsting@protonmail.com>

--- server/config/server.js
+++ server/config/server.js
@@ -7,15 +7,26 @@ const session = require('express-session');
 const passport = require('passport');
 const initializePassport = require('./passport');
 const morgan = require('morgan');
+const { rateLimit } = require('express-rate-limit');
 
 const app = express();
 const router = require('../routes');
 
 app.use(helmet());
 app.use(cors());
+
+app.use(
+	rateLimit({
+		windowMs: 60 * 60 * 1000, // 1 hour window
+		max: 100, // Limit each IP to 5 requests per hour
+		standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+		legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+		message: 'Hold up, The characters behind the scenes cant keep coming up with quotes',
+	})
+);
+
 app.use(compression());
 app.use(morgan('dev'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
