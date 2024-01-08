5cf9bae6e9e40a0d0194fd1a81a0b36ca205fa20
Rocktim Saikia <rocktimthedev@gmail.com>
2024-01-08 13:02:17 +0530

    chore: remove unused anime and quote schemas

--- server/db/schema/anime.ts
+++
@@ -1,5 +0,0 @@
-import { mysqlTable, text } from 'drizzle-orm/mysql-core';
-
-export const Anime = mysqlTable('anime', {
-	name: text('name'),
-});
--- server/db/schema/quote.ts
+++
@@ -1,7 +0,0 @@
-import { mysqlTable, text } from 'drizzle-orm/mysql-core';
-
-export const quote = mysqlTable('quote', {
-	anime: text('anime'),
-	character: text('character'),
-	quote: text('quote'),
-});
