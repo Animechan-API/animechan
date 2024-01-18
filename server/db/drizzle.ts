2bc8a6a90192d104e63407beeffe2ddcd7a709c5
Rocktim Saikia <rocktimthedev@gmail.com>
2024-01-18 11:48:06 +0530

    refactor(server): separate prod and dev db configs

--- server/db/drizzle.ts
+++ server/db/drizzle.ts
@@ -1,12 +1,26 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/planetscale-serverless';
+
 import { connect } from '@planetscale/database';
+import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import * as schema from '~/db/schema';
 
+const DATABASE_HOST =
+	process.env.NODE_ENV !== 'production'
+		? process.env.DEV_DATABASE_HOST
+		: process.env.PROD_DATABASE_HOST;
+const DATABASE_USERNAME =
+	process.env.NODE_ENV !== 'production'
+		? process.env.DEV_DATABASE_USERNAME
+		: process.env.PROD_DATABASE_USERNAME;
+const DATABASE_PASSWORD =
+	process.env.NODE_ENV !== 'production'
+		? process.env.DEV_DATABASE_PASSWORD
+		: process.env.PROD_DATABASE_PASSWORD;
+
 const connection = connect({
-	host: process.env.DATABASE_HOST,
-	username: process.env.DATABASE_USERNAME,
-	password: process.env.DATABASE_PASSWORD,
+	host: DATABASE_HOST,
+	username: DATABASE_USERNAME,
+	password: DATABASE_PASSWORD,
 });
 
 export const db = drizzle(connection, { schema, logger: true });
