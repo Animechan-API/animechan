d691e6c7cea3bce4cee82dba3e6298c089597cde
melosh <59763532+melosh101@users.noreply.github.com>
2024-01-24 11:04:24 +0100

    use the DATABASE URL to connect
    instead of using 3 credentials just use a single url for connecting to the database. i overlooked the url param on the planetscale database-js driver

--- server/db/drizzle.ts
+++ server/db/drizzle.ts
@@ -4,23 +4,13 @@ import { connect } from '@planetscale/database';
 import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import * as schema from '~/db/schema';
 
-const DATABASE_HOST =
+const DATABASE_URL =
 	process.env.NODE_ENV !== 'production'
-		? process.env.DEV_DATABASE_HOST
-		: process.env.PROD_DATABASE_HOST;
-const DATABASE_USERNAME =
-	process.env.NODE_ENV !== 'production'
-		? process.env.DEV_DATABASE_USERNAME
-		: process.env.PROD_DATABASE_USERNAME;
-const DATABASE_PASSWORD =
-	process.env.NODE_ENV !== 'production'
-		? process.env.DEV_DATABASE_PASSWORD
-		: process.env.PROD_DATABASE_PASSWORD;
+		? process.env.DEV_DATABASE_URL
+		: process.env.PROD_DATABASE_URL;
 
 const connection = connect({
-	host: DATABASE_HOST,
-	username: DATABASE_USERNAME,
-	password: DATABASE_PASSWORD,
+	url: DATABASE_URL
 });
 
 export const db = drizzle(connection, { schema, logger: true });
--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from 'drizzle-kit';
 
+const DATABASE_URL =
+	process.env.NODE_ENV !== 'production'
+		? process.env.DEV_DATABASE_URL
+		: process.env.PROD_DATABASE_URL;
+
 export default {
 	driver: 'mysql2',
 	schema: './db/schema.ts',
 	out: './migrations',
 	dbCredentials: {
-		uri: process.env.DATABASE_URL!,
+		uri: DATABASE_URL,
 	},
 	strict: true,
 } satisfies Config;
