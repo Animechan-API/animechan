517473b9fbb92a38930f392975fe2d16743fed7f
Rocktim Saikia <rocktimthedev@gmail.com>
2024-08-22 04:15:09 +0530

    build: add pm2 settings

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	apps: [
+		{
+			name: "animechan",
+			script: "pnpm",
+			args: "start",
+			watch: true,
+			ignore_watch: [
+				"**/node_modules",
+				"**/*.md",
+				".github",
+				".editorconfig",
+				".gitignore",
+				".prettierignore",
+				".env_example",
+				"client/.next",
+				"server/dist",
+				"LICENSE",
+				"logs",
+				".nvmrc",
+			],
+			autorestart: true,
+			max_memory_restart: "500M",
+			out_file: "logs/output.log",
+			error_file: "logs/error.log",
+			log_file: "logs/combined.log",
+		},
+	],
+};
