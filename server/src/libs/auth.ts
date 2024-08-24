712bfcee81b50447fc728a9d2c91dd2d8478cacf
Rocktim <rocktimthedev@gmail.com>
2024-08-25 02:58:06 +0530

    refactor(auth): streamline protected route validation logic (#117)
    * refactor(auth): streamline protected route validation logic
    
    * refactor(server): abstract auth logic and add vitest tests
    
    * test(auth): add case for protected endpoint with extra params
    
    * refactor(auth): use originalUrl to improve endpoint validation

--- server/src/libs/auth.ts
+++ server/src/libs/auth.ts
@@ -2,35 +2,44 @@ import type { NextFunction, Request, Response } from "express";
 import { prisma } from "~/libs/prisma";
 import { rateLimitOnApiKey, rateLimitOnIP } from "~/libs/rate-limit";
 import { redisClient } from "~/libs/redis";
-
-const PUBLIC_ENDPOINTS = [
-	"/api/v1/quotes/random",
-	"/api/v1/quotes/random/",
-	"/api/v1/quotes",
-	"/api/v1/quotes/",
-];
+import { isProtectedEndpoint, isValidEndpoint } from "./auth.util";
 
 export const protectedRoutes = async (req: Request, res: Response, next: NextFunction) => {
-	const isProtectedEndpoint = PUBLIC_ENDPOINTS.every((endpoint) => req.originalUrl !== endpoint);
-	// Basic auth checks before hitting the db for final verification
+	// If endpoint is not valid at all, return 404
+	// No need to hit the Redis nor the main db.
+	if (!isValidEndpoint(req.originalUrl)) {
+		return res.status(404).json({ message: "Endpoint not found" });
+	}
+
+	// Verification for protected endpoints
 	const reqApiKey = req.headers["x-api-key"] as string;
 
 	if (!reqApiKey) {
-		if (isProtectedEndpoint) {
+		// If no API key is provided and the requested endpoint is protected
+		// Return 401 immediately and end the request.
+		if (isProtectedEndpoint(req.originalUrl)) {
 			return res.status(401).json({ message: "Unauthorized. Missing API key!" });
 		}
+
+		// If no API key is provided and the requested endpoint is FREE
+		// Continue to the request with IP based rate limiting.
 		return rateLimitOnIP(req, res, next);
 	}
 
+	// Below we put all the validations if an potential API key
+	// is found in the incoming API request headers.
 	if (!reqApiKey.startsWith("ani-") || reqApiKey.length < 60) {
 		return res.status(401).json({ message: "Unauthorized. Invalid API key!" });
 	}
 
-	// Cache validate
+	// Check if the API key is already cached in Redis store.
+	// If it is that means, it is a valid key since we only store
+	// valid keys in Redis.
 	const cachedApiKey = await redisClient.get(`rl_api:${reqApiKey}`);
 
+	// If it is not in the cache, lastly check if it is in the database
+	// and add it to the cache if found. Else return 401.
 	if (!cachedApiKey) {
-		// Primary db validate
 		const apiKeyObj = await prisma.apiKey.findUnique({
 			where: { key: reqApiKey },
 		});
