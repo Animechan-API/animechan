35704c955d0bd34489242387ffcbb0148fdcc04d
Rocktim Saikia <rocktimthedev@gmail.com>
2024-08-29 18:31:28 +0530

    feat: standardize API responses

--- server/src/controllers/anime.ts
+++ server/src/controllers/anime.ts
@@ -1,17 +1,12 @@
 import type { Request, Response } from "express";
 import { prisma } from "~/libs/prisma";
+import { sendErrorResponse, sendSuccessResponse } from "./utils";
 
 export const getAnimeInformation = async (req: Request, res: Response) => {
 	const animeId = req.params?.id;
 
 	if (!animeId) {
-		return res.status(400).json({
-			status: "error",
-			error: {
-				code: 400,
-				message: "Not a valid 'animeId'",
-			},
-		});
+		return sendErrorResponse(res, { code: 400, message: "Not a valid 'animeId'" });
 	}
 
 	const anime = await prisma.anime.findUnique({
@@ -20,42 +15,30 @@ export const getAnimeInformation = async (req: Request, res: Response) => {
 	});
 
 	if (!anime) {
-		return res.status(404).json({
-			status: "error",
-			error: {
-				code: 404,
-				message: "No matching anime found",
-			},
-		});
+		return sendErrorResponse(res, { code: 404, message: "No matching anime found" });
 	}
 
 	const characters = anime.animeCharacters.map((c) => ({
 		id: c.id,
 		name: c.name,
 	}));
-	return res.json({
-		status: "success",
-		data: {
-			id: anime.id,
-			name: anime.name,
-			summary: anime.synopsis,
-			episodesCount: anime.episodeCount,
-			characters: characters,
-		},
-	});
+
+	const data = {
+		id: anime.id,
+		name: anime.name,
+		summary: anime.synopsis,
+		episodesCount: anime.episodeCount,
+		characters: characters,
+	};
+
+	return sendSuccessResponse(res, data);
 };
 
 export const getAnimeSummary = async (req: Request, res: Response) => {
 	const animeId = req.params?.id;
 
 	if (!animeId) {
-		return res.status(400).json({
-			status: "error",
-			error: {
-				code: 400,
-				message: "Not a valid 'animeId'",
-			},
-		});
+		return sendErrorResponse(res, { code: 400, message: "Not a valid 'animeId'" });
 	}
 
 	const anime = await prisma.anime.findUnique({
@@ -64,21 +47,13 @@ export const getAnimeSummary = async (req: Request, res: Response) => {
 	});
 
 	if (!anime) {
-		return res.status(404).json({
-			status: "error",
-			error: {
-				code: 404,
-				message: "No matching anime found",
-			},
-		});
+		return sendErrorResponse(res, { code: 404, message: "No matching anime found" });
 	}
 
-	return res.json({
-		status: "success",
-		data: {
-			id: anime.id,
-			name: anime.name,
-			summary: anime.synopsis,
-		},
-	});
+	const data = {
+		id: anime.id,
+		name: anime.name,
+		summary: anime.synopsis,
+	};
+	return sendSuccessResponse(res, data);
 };
