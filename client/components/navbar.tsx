b0320bad9f4f08378e413a56e73e56cf95c31e71
Rocktim Saikia <rocktimthedev@gmail.com>
2024-08-29 16:13:14 +0530

    Adapt navbar links for responsive design

--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -1,13 +1,16 @@
 import { RocketIcon } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
 import Link from "next/link";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 export default function Navbar() {
+	const windowSize = useWindowSize();
+
 	return (
 		<div className="shadow-md" id="navbar">
 			<nav className="container relative lg:mx-auto px-2 max-w-4xl py-3 navbar-expand-lg mb-3 text-green-700">
 				<div className="container mx-auto flex flex-wrap items-center justify-between">
-					<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+					<div className="md:w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
 						<Link href="/">
 							<Text weight="medium" className="cursor-default">
 								Animechan
@@ -19,7 +22,7 @@ export default function Navbar() {
 						<Link href="/docs">
 							<Button variant="ghost">
 								<Text weight="medium" size="3">
-									Documentation
+									{windowSize.width <= 500 ? "Docs" : "Documentation"}
 								</Text>
 							</Button>
 						</Link>
