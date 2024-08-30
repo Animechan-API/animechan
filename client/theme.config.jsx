1bf83390f01969a3a21d66dab67d449fa5b326f7
Rocktim Saikia <rocktimthedev@gmail.com>
2024-08-30 22:08:34 +0530

    Update logo with Text component and styling

--- client/theme.config.jsx
+++ client/theme.config.jsx
@@ -1,8 +1,14 @@
+import { Text } from "@radix-ui/themes";
+
 export default {
 	nextThemes: {
 		defaultTheme: "light",
 	},
-	logo: <span>Animechan</span>,
+	logo: (
+		<Text weight="medium" className="cursor-default" color="green">
+			Animechan
+		</Text>
+	),
 	project: {
 		link: "https://github.com/Animechan-API/animechan",
 	},
