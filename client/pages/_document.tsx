1db9636fb469137fc2de96756a3d6aadb78333b3
Rocktim <rocktimthedev@gmail.com>
2024-12-20 16:56:46 +0530

    Remove redundanrt adsense script

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -8,11 +8,6 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
 					rel="stylesheet"
 				/>
-				<script
-					async
-					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5475485771562496"
-					crossOrigin="anonymous"
-				/>
 				<script async src="https://cdn.splitbee.io/sb.js" />
 			</Head>
 			<body>
